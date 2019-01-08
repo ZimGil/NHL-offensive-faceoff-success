8fc36a5a5b8247a0de3f1060b3de1e122e5c3cf8
ZimGil <gil.tichon@gmail.com>
2019-01-08 19:59:40 +0200

    better error handle

--- index.js
+++ index.js
@@ -8,17 +8,16 @@ const season = process.argv[2];
 
 if (!season || !season.match(/^\d{8}$/g)) {
   console.log(colors.red('Please provide a valid season in 8 digits pattern (i.g: 20172018)'));
-  process.exit();
+} else {
+  fetchSeasonData(season)
+    .then(gamesData => {
+      console.log(colors.cyan(`Done fetching data for season ${season}\nAnalyzing...`));
+      return analyseSeasonData(gamesData);
+    })
+    .then(offensiveFaceoffWinRatio => {
+      console.log([
+        colors.green(`Offensive Faceoff Win Ratio for the ${season} season:`),
+        colors.cyan(offensiveFaceoffWinRatio)
+      ].join(' '));
+    });
 }
-
-fetchSeasonData(season)
-  .then(gamesData => {
-    console.log(colors.cyan(`Done fetching data for season ${season}\nAnalyzing...`));
-    return analyseSeasonData(gamesData);
-  })
-  .then(offensiveFaceoffWinRatio => {
-    console.log([
-      colors.green(`Offensive Faceoff Win Ratio for the ${season} season:`),
-      colors.cyan(offensiveFaceoffWinRatio)
-    ].join(' '));
-  });
